d1ca795c0709779f6ad4bca8488b98b7f6b3bb22
shrfrn <shrfrn@gmail.com>
2024-08-20 21:33:08 +0300

    Init repo

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <AppHeader />
+    <main>
+        <RouterView />
+    </main>
+    <AppFooter />
+</template>
+
+<script>
+    import AppHeader from './cmps/AppHeader.vue'
+    import AppFooter from './cmps/AppFooter.vue'
+    export default {
+        components: {
+            AppHeader,
+            AppFooter,
+        }
+    }
+</script>
+
+<style scoped>
+main {
+    min-height: calc(90vh - 1rem);
+    padding: .5em;
+}
+</style>
